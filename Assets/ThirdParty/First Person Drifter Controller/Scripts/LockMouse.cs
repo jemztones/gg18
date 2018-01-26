d3094d28e9592a8b6a34597077d7d1d61969a0c6
jemztones <jsmchugh@gmail.com>
2018-01-26 23:29:09 +1100

    FPS Drifter with lock updated to remove references to deprecated methods.

---
+++ Assets/ThirdParty/First Person Drifter Controller/Scripts/LockMouse.cs
@@ -0,0 +1,39 @@
+﻿// by @torahhorse
+
+using UnityEngine;
+using System.Collections;
+
+public class LockMouse : MonoBehaviour
+{	
+	void Start()
+	{
+		LockCursor(CursorLockMode.Locked);
+	}
+
+	void Update()
+	{
+		// lock when mouse is clicked
+		if( Input.GetMouseButtonDown(0) && Time.timeScale > 0.0f )
+		{
+			LockCursor(CursorLockMode.Locked);
+		}
+	
+		// unlock when escape is hit
+		if  ( Input.GetKeyDown(KeyCode.Escape) )
+		{
+            if (Cursor.lockState == CursorLockMode.None)
+            {
+                LockCursor(CursorLockMode.Locked);
+            }
+            else
+            {
+                LockCursor(CursorLockMode.None);
+            }
+		}
+	}
+
+    public void LockCursor(CursorLockMode cursorLockMode)
+    {
+        Cursor.lockState = cursorLockMode;
+	}
+}
