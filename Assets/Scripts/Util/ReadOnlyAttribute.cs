e75ea827035bbccd16cf4428606bc3b8a10b6de5
jemztones <jsmchugh@gmail.com>
2018-01-26 22:53:45 +1100

    Read only attribute & drawer

---
+++ Assets/Scripts/Util/ReadOnlyAttribute.cs
@@ -0,0 +1,8 @@
+﻿// From http://answers.unity3d.com/questions/489942/how-to-make-a-readonly-property-in-inspector.html
+
+using UnityEngine;
+
+public class ReadOnlyAttribute : PropertyAttribute
+{
+
+}
