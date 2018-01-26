0c1aa10d520879c1722e0e3f1e1a94cfcd6f0042
jemztones <jsmchugh@gmail.com>
2018-01-27 00:20:01 +1100

    Infection point damage increase customisable.

--- Assets/Scripts/InfectionPoint.cs
+++ Assets/Scripts/InfectionPoint.cs
@@ -2,9 +2,12 @@
 
 public class InfectionPoint : MonoBehaviour
 {
+	[SerializeField]
+	int infectionAmount;
+
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("i'm infected");
-        other.gameObject.GetComponent<InfectionStatus>().IncreaseInfectionLevel(10);
+		other.gameObject.GetComponent<InfectionStatus>().IncreaseInfectionLevel(infectionAmount);
     }
 }
