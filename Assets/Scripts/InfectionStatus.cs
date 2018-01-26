bbbcca21f064063ba3cd2c8a64386e35795a520a
jemztones <jsmchugh@gmail.com>
2018-01-26 22:55:29 +1100

    Infection status script

---
+++ Assets/Scripts/InfectionStatus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfectionStatus : MonoBehaviour
+{
+    [Header("Setup")]
+
+    [SerializeField]
+    int initialInfectionLevel = 0;
+
+    [Header("State")]
+
+    [ReadOnly]
+    [SerializeField]
+    int infectionLevel;
+
+    private void Start()
+    {
+        infectionLevel = initialInfectionLevel;
+    }
+}
