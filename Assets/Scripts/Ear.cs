a531637d5548a4b9bb5069006a1a2eae08bc4f3d
jemztones <jsmchugh@gmail.com>
2018-01-28 01:33:15 +1100

    Ear - turn off sound when hand is over

---
+++ Assets/Scripts/Ear.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class Ear : VRTK_InteractableObject
+{
+	[Header("Ear")]
+
+	[ReadOnly]
+	[SerializeField]
+	bool covered = false;
+
+	[SerializeField]
+	AudioListener listener;
+
+	public override void StartTouching(VRTK_InteractTouch currentTouchingObject = null)
+	{
+		Debug.Log ("covered ear");
+		covered = true;
+		listener.enabled = false;
+		base.StartTouching(currentTouchingObject);
+	}
+
+	public override void StopTouching(VRTK_InteractTouch previousTouchingObject = null)
+	{
+		Debug.Log ("uncovered ear");
+		covered = false;
+		listener.enabled = true;
+		base.StopTouching(previousTouchingObject);
+	}
+}
