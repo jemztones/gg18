cb0d047ce71d9a441751b5c815a8322946004f5b
jemztones <jsmchugh@gmail.com>
2018-01-27 11:28:00 +1100

    Player State which finds all infection statuses in the scene.

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+	InfectionStatus[] infectionStatuses;
+
+	// Use this for initialization
+	void Start ()
+	{
+		InfectionStatus[] infectionStatuses = FindObjectsOfType<InfectionStatus> ();
+		foreach (InfectionStatus infectionStatus in infectionStatuses)
+		{
+			//infectionStatus.infectionChangeEvent.AddListener();
+		}
+	}
+}
