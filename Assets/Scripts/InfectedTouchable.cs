6e3ca499fe1fd33429ef339a1a0698fcfa918e33
jemztones <jsmchugh@gmail.com>
2018-01-27 17:46:58 +1100

    Infection touchable apply infection over time

--- Assets/Scripts/InfectedTouchable.cs
+++ Assets/Scripts/InfectedTouchable.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using VRTK;
 
 [RequireComponent(typeof(AudioSource))]
@@ -7,11 +8,26 @@ public class InfectedTouchable : VRTK_InteractableObject
 	[Header("Infection")]
 
 	[SerializeField]
+	int infectionAmount;
+
+	[SerializeField]
+	float infectionTick;
+
+	[SerializeField]
 	AudioSource audioSource;
+
+	InfectionStatus infectionStatus;
+
+	Coroutine infectionCoroutine;
 
     public override void StartTouching(VRTK_InteractTouch currentTouchingObject = null)
     {
 		audioSource.Play ();
+		infectionStatus = currentTouchingObject.GetComponent<InfectionStatus> ();
+		if (infectionStatus)
+		{
+			infectionCoroutine = StartCoroutine(InfectionTouchRoutine());
+		}
         base.StartTouching(currentTouchingObject);
         Debug.Log("i'm being infected");
     }
@@ -20,5 +36,22 @@ public class InfectedTouchable : VRTK_InteractableObject
     {
         base.StopTouching(previousTouchingObject);
         Debug.Log("i'm not touching anything infected");
+		if (infectionCoroutine != null)
+		{
+			Debug.Log("Stop infection coroutine");
+			StopCoroutine (infectionCoroutine);
+			infectionStatus = null;
+			infectionCoroutine = null;
+		}
     }
+
+	IEnumerator InfectionTouchRoutine()
+	{
+		while (true)
+		{
+			audioSource.Play ();
+			infectionStatus.IncreaseInfectionLevel(infectionAmount);
+			yield return new WaitForSeconds (infectionTick);
+		}
+	}
 }
