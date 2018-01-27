83093953a4b2987fe34197dfbd0929b7515ef315
jemztones <jsmchugh@gmail.com>
2018-01-27 11:25:35 +1100

    Infection over time happens using coroutine.

--- Assets/Scripts/InfectionPoint.cs
+++ Assets/Scripts/InfectionPoint.cs
@@ -1,13 +1,42 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class InfectionPoint : MonoBehaviour
 {
 	[SerializeField]
 	int infectionAmount;
 
+	[SerializeField]
+	float infectionTick;
+
+	InfectionStatus infectionStatus;
+
+	Coroutine infectionCoroutine;
+
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("i'm infected");
-		other.gameObject.GetComponent<InfectionStatus>().IncreaseInfectionLevel(infectionAmount);
+        Debug.Log("i'm being infected");
+		infectionStatus = other.gameObject.GetComponent<InfectionStatus> ();
+		infectionCoroutine = StartCoroutine (InfectionTouchRoutine());
     }
+
+	void OnTriggerExit()
+	{
+		if (infectionCoroutine != null)
+		{
+			Debug.Log("Stop infection coroutine");
+			StopCoroutine (infectionCoroutine);
+			infectionStatus = null;
+			infectionCoroutine = null;
+		}
+	}
+
+	IEnumerator InfectionTouchRoutine()
+	{
+		while (true)
+		{
+			infectionStatus.IncreaseInfectionLevel(infectionAmount);
+			yield return new WaitForSeconds (infectionTick);
+		}
+	}
 }
