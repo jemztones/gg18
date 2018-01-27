6446535d0639ea297f44b0ae47a6cee0237df0d4
jemztones <jsmchugh@gmail.com>
2018-01-27 15:40:01 +1100

    Infection touchcable built off vrtk

---
+++ Assets/Scripts/InfectedTouchable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using VRTK;
+
+
+public class InfectedTouchable : VRTK_InteractableObject
+{
+    public override void StartTouching(VRTK_InteractTouch currentTouchingObject = null)
+    {
+        base.StartTouching(currentTouchingObject);
+        Debug.Log("i'm being infected");
+    }
+
+    public override void StopTouching(VRTK_InteractTouch previousTouchingObject = null)
+    {
+        base.StopTouching(previousTouchingObject);
+        Debug.Log("i'm not touching anything infected");
+    }
+}
