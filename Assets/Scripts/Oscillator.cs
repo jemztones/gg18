4887a38d724542c904493ea222371ddff79d4a34
tkzjustin <tamtzekin@gmail.com>
2018-01-28 01:21:38 +1100

    New audio mixdown, fixed lighting, updated oscillator for light source.

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Oscillator : MonoBehaviour {
+	float timeCounter=0;
+
+	public float speed;
+	public float width;
+	public float height;
+
+	Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+		speed = 1;
+		width = 60;
+		height = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeCounter += Time.deltaTime*speed;
+
+		float x = Mathf.Cos (timeCounter)*width;
+		float y = 0;
+		float z = Mathf.Sin (timeCounter)*height;
+		
+		transform.position = new Vector3 (x, y, z) + startPos;
+	
+	}
+}
